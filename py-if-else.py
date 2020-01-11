7f4a146cda1d459db175abe8d3d108d257db248b
enjang <enjang.s@ovo.id>
2020-01-11 22:56:48 +0700

    soal if then else

--- py-if-else.py
+++ py-if-else.py
@@ -1,11 +1,13 @@
 #!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
 
-
-if __name__ == '__main__':
-    n = int(input().strip())
+n = int(input("Enter a number: "))
+if (n % 2) == 0:
+     if ( 2 <= n <= 5 ): 
+        print ("Not Weird")
+     elif ( 6 <= n <= 20 ):
+        print("Weird")
+     else:
+        print("Not Weird")
+else:
+    print("Weird")
