582934453164aed98b29aa67afd7561c53d31153
enjang <enjang.s@ovo.id>
2020-01-12 00:17:42 +0700

    add new code

---
+++ py-arithmetic.py
@@ -0,0 +1,13 @@
+#!/usr/bin python3
+
+if __name__ == '__main__':
+   a = int(input("Masukkan angka pertama: "))
+   b = int(input("Masukkan angka kedua: "))
+
+sum = a+b
+dif = a-b
+prod = a*b
+
+print("Hasil Pertambahan " + str(sum))
+print("Hasil Pengurangan " + str(dif))
+print("Hasil Perkalian " +  str(prod))
